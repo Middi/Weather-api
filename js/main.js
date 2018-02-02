80f40d231878ee3a277c88dab95ce7be6b28f776
“Richard Middleton” <Richard@richardmiddleton.me>
2018-02-02 22:20:02 +0000

    update dom with static coords

---
+++ js/main.js
@@ -0,0 +1,37 @@
+$(document).ready(function () {
+
+    var endPoint = 'http://api.openweathermap.org/data/2.5/forecast?';
+    var lat = 'lat=' + '53.707105';
+    var lon = '&lon=' + '-1.243742';
+    var APIKey = 'APPID=' + '1f3e30098d59daa0ee84d36dca533728';
+    var settings = '&units=metric&'
+
+    var URL = endPoint + lat + lon + settings + APIKey;
+
+    function weather() {
+        $.getJSON(URL, (data) => {
+            console.log(data);
+            updateDOM(data);
+        });
+    }
+    weather();
+
+    function updateDOM(data) {
+        var city = data.city.name + ', ' + data.city.country;
+        var temp = Math.round(data.list[0].main.temp);
+        var wind = data.list[0].wind.speed;
+        var humid = data.list[0].main.humidity;
+        var desc = data.list[0].weather[0].description;
+
+        $('#city').html(city);
+        $('#temp').html(temp);
+        $('#wind').html(' ' + wind + 'mph');
+        $('#humid').html(' ' + humid + '%');
+        $('#desc').html(' '+ desc);
+
+        $('.day-temp').each(function(i, day) {
+            $(day).html(Math.round(data.list[i].main.temp) + '&#176;');
+        });
+    }
+
+});
