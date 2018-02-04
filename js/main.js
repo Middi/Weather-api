6a7203c8a8ba74154edafc3718800691587125c0
“Richard Middleton” <Richard@richardmiddleton.me>
2018-02-04 16:12:27 +0000

    stripped down

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 $(document).ready(function () {
 
+    // Get Location
     navigator.geolocation.getCurrentPosition(success, error);
 
     function success(pos) {
@@ -8,12 +9,7 @@ $(document).ready(function () {
         weather(latitude, longitude);
     }
 
-    function error() {
-        console.log('error');
-    }
-
-    var celsius = true;
-
+    // Call Weather
     function weather(latitude, longitude) {
         var APIKey = 'APPID=' + '76c3222b05bd5fe6e2f602b4591b0aec';
         var settings = '&units=metric&';
@@ -26,64 +22,21 @@ $(document).ready(function () {
 
     // Update DOM
     function updateDOM(data) {
+        // Main Data
         var city = data.city.name + ', ' + data.city.country;
-        var wind = data.list[0].speed;
-        var humid = data.list[0].humidity;
         var desc = data.list[0].weather[0].description;
-        var icon = 'http://openweathermap.org/img/w/10d.png';
-
         $('#city').html(city);
-        $('#wind').html(' ' + wind + 'mph');
-        $('#humid').html(' ' + humid + '%');
         $('#desc').html(' ' + desc);
-        $('#icon').attr('src', icon);
-        console.log(data);
 
+        // Looped Data
         $('.day').each(function (i, day) {
-            var dayIcon = data.list[i].weather[0].icon;
+            var icon = data.list[i].weather[0].icon;
             var today = new Date().getTime();
             var hours = new Date(today + ((i) * 86400000));
 
             $(day).find('.date').html(hours.toString().split(' ')[0]);
             $(day).find('.day-temp').html(Math.round(data.list[i].temp.day) + '&#176;');
-            $(day).find('.day-icon').attr('src', 'http://openweathermap.org/img/w/' + dayIcon + '.png');
+            $(day).find('.icon').attr('src', 'http://openweathermap.org/img/w/' + icon + '.png');
         });
     }
-
-
-    // Farenheit conversion
-
-    $('#c-deg').on('click', () => {
-        if (!celsius) {
-            var tempF = $('#temp').html();
-            $('#temp').html(Math.round((tempF - 32) * 0.5556));
-            // 5 Days Conversion
-            $('.day-temp').each(function (i, day) {
-                var tempD = $(day).html().replace('°', '');
-                $(day).html(Math.round((tempD - 32) * 0.5556) + '&#176;');
-            });
-            $('#c-deg').addClass('button-active');
-            $('#f-deg').removeClass('button-active');
-            celsius = true;
-        }
-    });
-
-    $('#f-deg').on('click', () => {
-        if (celsius) {
-            var tempF = $('#temp').html();
-            $('#temp').html(Math.round((tempF * 1.8000) + 32));
-            // 5 Days Conversion
-            $('.day-temp').each(function (i, day) {
-                var tempD = $(day).html().replace('°', '');
-                $(day).html(Math.round((tempD * 1.8000) + 32) + '&#176;');
-            });
-            $('#f-deg').addClass('button-active');
-            $('#c-deg').removeClass('button-active');
-            celsius = false;
-        }
-    });
-
-    // Dates
-
-
 });
